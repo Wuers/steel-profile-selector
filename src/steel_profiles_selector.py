8bd29908aeca6a6b854430d0af07b303e4ef5049
Wueros <wiktor.sadowski98@gmail.com>
2024-09-24 22:22:52 +0200

    adding venv

--- src/steel_profiles_selector.py
+++ src/steel_profiles_selector.py
@@ -10,7 +10,7 @@ def choose_profile(span, profiles_df):
     suitable_profiles = []
     for index, profile in profiles_df.iterrows():
     #first calculate loads for self_weight of given profile
-        self_weight=structural_analysis.calculate_self_weight(profile['weight(kg/m)'])
+        self_weight=structural_analysis.calculate_self_weight(profile['Weight'])
         shared_data.dead_load=shared_data.dead_load_base+self_weight
     #calculation of loads:
         total_load = load_calculator.load_sum(
@@ -24,17 +24,17 @@ def choose_profile(span, profiles_df):
         vertical_force = structural_analysis.calculate_vertical_force(combination_load, shared_data.span)
     
     #calculation max forces in given steel section:
-        mcrd_moment = structural_analysis.calculate_Mcrd_moment(profile['plastic_section_modulus(cm^3)'], profile['steel_grade'])
+        mcrd_moment = structural_analysis.calculate_Mcrd_moment(profile['Elastic section modulus'], profile['Steel grade'])
         vcrd_vertical_force = structural_analysis.calculate_Vcrd_vertical_force(
-            profile['section_area(cm^2)'],profile["b"],
-            profile["tf"],profile["tw"],profile["r"], profile['steel_grade'])
+            profile['Area'],profile["Depth"],
+            profile["Flange thickness"],profile["Web thickness"],profile["Root radius"], profile['Steel grade'])
 
         #if mcrd_moment > moment and vcrd_vertical_force > vertical_force:
         if True: 
             deflection = structural_analysis.calculate_deflection(
-                total_load, span, shared_data.E, profile["moment_of_inertia_Iy(cm^4)"])
+                total_load, span, shared_data.E, profile["Second moment of area"])
             
-            print(f'\n testing: {profile["profile_type"]} Mcrd: {mcrd_moment}, moment: {moment} Vcrd: {vcrd_vertical_force}, V: {vertical_force}, deflection: {deflection} ')
+            print(f'\n testing: {profile["Profile"]} Mcrd: {mcrd_moment}, moment: {moment} Vcrd: {vcrd_vertical_force}, V: {vertical_force}, deflection: {deflection} ')
             print(f"total_load: {total_load}, combination load: {combination_load}")
             print(f"max def: {shared_data.max_deflection}")
             if deflection < shared_data.max_deflection:
